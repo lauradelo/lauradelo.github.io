4e3262c720adcf22154b1c0b234d3326eab7a330
lauradelo <laura.deloffre@etudiant.univ-perp.fr>
2024-11-25 14:23:44 +0100

    test2

--- script.js
+++
@@ -1,5 +0,0 @@
-// Si tu veux ajouter des interactions, tu peux le faire ici.
-document.addEventListener('DOMContentLoaded', function() {
-    const herisson = document.querySelector('.herisson');
-    // Code JavaScript si tu veux manipuler le hérisson plus tard.
-});
